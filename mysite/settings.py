cb848f34e96eced5c384d80da2fd87670fa839ed
Shadman <shadmanchowdhury87@gmail.com>
2020-04-13 20:28:34 +0600

    Initial Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'exg1+swx6)!wa&15sr@ia2=^ir(aax_9umhr3rck&1sfy%&86j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-site69.herokuapp.com','127.0.0.1']
 
 
 # Application definition
